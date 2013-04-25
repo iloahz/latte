fda433bd6b01a86fa33328461a4afe7fb3a09567
Li Zhao <iloahz@gmail.com>
2013-04-25 20:43:47 +0800

    changed fetch timeout and added max total try time

--- contest/function.py
+++ contest/function.py
@@ -1,13 +1,17 @@
 import urllib2
+import time
 
 class proto():
     pass
 
 def fetch(url):
+    start = time.time()
     while True:
         try:
             req = urllib2.Request(url)
-            res = urllib2.urlopen(req, timeout=10000).read()
+            res = urllib2.urlopen(req, timeout=10).read()
             return res
         except:
-            pass
+            end = time.time()
+            if end - start > 60:
+                return ''
